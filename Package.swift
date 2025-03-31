041ad3dbe5cfb4c1a75037cd576aa9aa29838d1d
Andrew Roan <roanutil@users.noreply.github.com>
2025-03-31 11:28:54 -0500

    pre-release
    feature/v13.1.0-48

--- Package.swift
+++ Package.swift
@@ -19,22 +19,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BlackBerryCerticom",
-            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48/BlackBerryCerticom.xcframework.zip",
+            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48p/BlackBerryCerticom.xcframework.zip",
             checksum: "4c0d930586fdbfeab6512f8bd129b3309a10ffadf6591be7ea0494e7fc701218"
         ),
         .binaryTarget(
             name: "BlackBerryCerticomSBGSE",
-            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48/BlackBerryCerticomSBGSE.xcframework.zip",
+            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48p/BlackBerryCerticomSBGSE.xcframework.zip",
             checksum: "08f6d4a0607ff97f87732d1da87a1f3cf4e34c699b448f2c5abb08630d52e344"
         ),
         .binaryTarget(
             name: "BlackBerryDynamics",
-            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48/BlackBerryDynamics.xcframework.zip",
+            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48p/BlackBerryDynamics.xcframework.zip",
             checksum: "8d74ab2bbae407745fd2d34bef2ba44ebabdd004963e0e8d8cc8ec9361d5f992"
         ),
         .binaryTarget(
             name: "BlackBerryDynamicsAutomatedTestSupportLibrary",
-            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48/BlackBerryDynamicsAutomatedTestSupportLibrary.xcframework.zip",
+            url: "https://github.com/MFB-Technologies-Inc/blackberry-dynamics-ios-sdk/releases/download/13.1.0-48p/BlackBerryDynamicsAutomatedTestSupportLibrary.xcframework.zip",
             checksum: "95eaca8062aa648a1d79400bce7b56b141ccaf656604b684bac2f40ba058553c"
         ),
     ]
